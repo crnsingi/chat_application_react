07a10928e7f3ec4aab3917d7a67c8a38eaaaffbe
Cesar <cesarnsingi@hotmail.com>
2025-02-15 20:56:38 +0200

    const is first message by user

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,4 +1,5 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message}) => {
+    const isFirstMessageByUser = ! lastMessage
     return (
         <div>
             TheirMessage
