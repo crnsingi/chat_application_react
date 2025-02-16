cd6e333c2b009208d9b12e49dcd21f9aff68c3b5
Cesar <cesarnsingi@hotmail.com>
2025-02-16 05:14:27 +0200

    input property

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,12 +1,18 @@
 const MessageForm = () => { 
     const handleSubmit = () => {
-        
+
     }
+
+     
     return (
-
-       <form className="message-form" onSubmit={handleSubmit}>
-
-       </form>
+        <form className="message-form" onSubmit={handleSubmit}>
+          <input 
+              className="message-input"
+              placeholder="Send a message..."
+              value={value}
+              onChange={handleChange}
+            />
+        </form>
     );
 }
 
