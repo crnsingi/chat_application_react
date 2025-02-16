ccef0a2572aca94437372f78fd0b8bb943a6b462
Cesar <cesarnsingi@hotmail.com>
2025-02-16 05:44:26 +0200

    import send outlined and price outlined

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons'
 
 const MessageForm = (props) => { 
     const [value, setValue] = useState('');
