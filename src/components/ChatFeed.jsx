45d53ff6fa0e254ece36f732251ff3d34597dadf
Cesar <cesarnsingi@hotmail.com>
2025-02-16 09:43:10 +0200

    const render read receipts

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,12 +4,19 @@ import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
-
+  
     const chat = chats && chats[activeChat];
-
-    const renderReadReceipts = (message, isMyMessage) => {
-        chat.people.map((person, index) => person.last_read)
-    }
+  
+    const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+      <div
+        key={`read_${index}`}
+        className="read-receipt"
+        style={{
+          float: isMyMessage ? 'right' : 'left',
+          backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+        }}
+      />
+    ));
 
    const renderMessages = () => {
     const keys = Object.keys(messages);
