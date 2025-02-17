f2a329274e310fb83a14a6c9b62714e0a3549271
Cesar <cesarnsingi@hotmail.com>
2025-02-17 09:59:35 +0200

    return error property

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -28,20 +28,23 @@ const Modal = () => {
           }
         };
 
-    return ( 
-        <div className="wrapper">
-            <div className="form">
-                <h1 className="title">Chat Applicqtion</h1>
+        return (
+            <div className="wrapper">
+              <div className="form">
+                <h1 className="title">Chat Application</h1>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
-                    <div align="center">
-                        <button type="submit" className="button">
-                            <span>Start Chatting</span>
-                        </button>
-                    </div>
+                  <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                  <div align="center">
+                    <button type="submit" className="button">
+                      <span>Start chatting</span>
+                    </button>
+                  </div>
                 </form>
+                <h1>{error}</h1>
+              </div>
             </div>
-        </div>
-    );
-}
+        
+          );
+        };
+        
